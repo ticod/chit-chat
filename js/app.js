c5dcec99c605b07c3eca1d8275a7f5eeb5626263
tico <58782736+ticod@users.noreply.github.com>
2020-11-21 00:53:31 +0900

    Add: 드롭다운 메뉴, 레이아웃 크기 수정

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const dropdownmenu = document.getElementById('dropdownmenu');
+const dropdown = document.getElementById('dropdown');
+const subdropdown = document.querySelectorAll('.js_subdropdown');
+const subdropdownicon = document.querySelectorAll('.js_subdropdownIcon');
+const overlay = document.getElementById('overlay');
+
+dropdown.style.transition = '0.3s';
+
+let isOpen = false;
+
+function handleClick(event) {
+  if (isOpen) {
+    dropdownmenu.classList.add('dropdownmenu_close');
+    dropdownmenu.classList.remove('dropdownmenu_open');
+    dropdown.style.transform = 'rotate(0)';
+    overlay.style.display = 'none';
+  } else {
+    dropdownmenu.classList.add('dropdownmenu_open');
+    dropdownmenu.classList.remove('dropdownmenu_close');
+    dropdown.style.transform = 'rotate(90deg)';
+    overlay.style.display = 'block';
+  }
+  isOpen = !isOpen;
+}
+
+function handleMouseOver(event) {
+  event.target.querySelector('.js_subdropdownIcon').style.transform = 'rotate(-90deg)';
+}
+
+function handleMouseOut(event) {
+  event.target.querySelector('.js_subdropdownIcon').style.transform = 'rotate(0)';
+}
+
+dropdown.addEventListener('click', handleClick);
+
+for (const s in subdropdown) {
+  if (!isNaN(parseInt(s))) {
+    subdropdown[s].style.transition = '0.3s';
+    subdropdownicon[s].style.transition = '0.3s';
+    subdropdown[s].addEventListener('mouseover', handleMouseOver);
+    subdropdown[s].addEventListener('mouseout', handleMouseOut);
+  }
+}
